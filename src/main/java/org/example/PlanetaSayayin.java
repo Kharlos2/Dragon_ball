b624f4e7f31796e9c62a9556b71ed297e0e3cc7d
JoseaSosa <josea_1998@htomail.com>
2023-03-09 10:21:10 -0500

    se crea ejemplo clase abstracta

---
+++ src/main/java/org/example/PlanetaSayayin.java
@@ -0,0 +1,17 @@
+package org.example;
+
+public class PlanetaSayayin {
+    public static void main(String[] args) {
+
+        //Creando un objeto clase alta
+
+        ClaseAlta guerrero1 = new ClaseAlta("vegeta",90,30,"Negro");
+        guerrero1.pelear();
+
+        //Creando un objeto clase baja
+
+        ClaseBaja guerrero2 = new ClaseBaja("Goku",80,25,"Negro");
+        guerrero2.pelear();
+
+    }
+}
